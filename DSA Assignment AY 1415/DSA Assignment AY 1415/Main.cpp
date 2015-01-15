b95a51d42a034cbb9b6e13227dd4214dd3960dea
jeffreyyrey <jeffreyyrey@gmail.com>
2015-01-15 14:50:25 +0800

    Added main page and menu

---
+++ DSA Assignment AY 1415/DSA Assignment AY 1415/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "SortedArrayList.h"
+#include "UnsortedArrayList.h"
+#include "UnsortedPointerList.h"
+
+using namespace std;
+
+main()
+{
+	cout << "Welcome to DSA Assignment AY1415" << endl;
+	cout << "====================================" << endl; //36 =
+	cout << "What would you like to do?" << endl;
+	cout << "1) Import the songs into the program" << endl;
+	cout << "2) Add new song" << endl;
+	cout << "3) Remove a song" << endl;
+	cout << "4) Search a song using Sequential Search" << endl;
+	cout << "5) Search a song using Binary Search" << endl;
+	cout << "0) End the program" << endl;
+}
