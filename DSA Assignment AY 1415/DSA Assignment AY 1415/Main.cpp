083d0034f61faa898269c2acd0e63cb4565641ed
SyafiqahR <nursyafiqah.rozaini@gmail.com>
2015-01-20 01:24:24 +0800

    Deleted ReadingFile header&Cpp, fixed menu

--- DSA Assignment AY 1415/DSA Assignment AY 1415/Main.cpp
+++ DSA Assignment AY 1415/DSA Assignment AY 1415/Main.cpp
@@ -1,54 +1,82 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "SortedArrayList.h"
 #include "UnsortedArrayList.h"
 #include "UnsortedPointerList.h"
-#include "ReadingFile.h"
-
+//#include "ReadingFile.h"
 
 using namespace std;
 
-int choiceSelection(int option)
+int readSongs()
 {
-	switch (option)
+	int noOfSongs, count = 0;
+	string song;
+	cout << "How many songs would you like to import?" << endl;
+	cin >> noOfSongs;
+	/*ifstream file("mxm_779k_matches");
+	while (std::getline(file, song))
 	{
-	case '1':
-		//Import songs
-		int readSongs();
-		break;
-	case '2':
-		//Add songs
-		break;
-	case '3':
-		//Remove songs
-		break;
-	case '4':
-		//Search using sequential search
-		break;
-	case '5':
-		//search using binary search
-		break;
-	case '0':
-		//exit program
-		break;
-	}
+		//Process
+		//commands to read a specific number of lines
+		if (noOfSongs > count)
+		{
+			if (song.at(0) != '#')
+			{
+				//store it in an array
+				count++;
+				cout << count << " song" << endl; //testing only
+			}
+		}
+		else
+			false; //stop the reading
+		
+	}*/
 	return 0;
 }
 
 int main()
 {
-	cout << "Welcome to DSA Assignment AY1415" << endl;
-	cout << "====================================" << endl; //36 =
-	cout << "What would you like to do?" << endl;
-	cout << "1) Import the songs into the program" << endl;
-	cout << "2) Add new song" << endl;
-	cout << "3) Remove a song" << endl;
-	cout << "4) Search a song using Sequential Search" << endl;
-	cout << "5) Search a song using Binary Search" << endl;
-	cout << "0) End the program" << endl;
-
-	int option;
-	cin >> option;
-	choiceSelection(option);
-
-	return 0;
+	int option = -1; //default
+	while (option != 0)
+	{
+		cout << "" << endl;
+		cout << "Welcome to DSA Assignment AY1415" << endl;
+		cout << "====================================" << endl;
+		cout << "What would you like to do?" << endl;
+		cout << "1) Import the songs into the program" << endl;
+		cout << "2) Add new song" << endl;
+		cout << "3) Remove a song" << endl;
+		cout << "4) Search a song using Sequential Search" << endl;
+		cout << "5) Search a song using Binary Search" << endl;
+		cout << "0) End the program" << endl;
+		cout << "Enter your option : ";
+		cin >> option;
+		cout << "" << endl;
+		switch (option)
+		{
+		case 0:
+			option = 0;
+			break;
+		case 1:
+			//Import songs
+			readSongs();
+			break;
+		case 2:
+			//Add songs
+			break;
+		case 3:
+			//Remove songs
+			break;
+		case 4:
+			//Search using sequential search
+			break;
+		case 5:
+			//search using binary search
+			break;
+		default:
+			cout << "Invalid Option! Please choose again." << endl;
+			break;
+		}
+	}
 }
